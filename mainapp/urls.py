93a54d0e4f792a652684a85dc1f18cb09185ede5
egorfw <egorfw@outlook.com>
2023-02-24 16:53:44 +0300

    lesson_5 Authapp done

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("", views.MainPageView.as_view(), name="main_page"),
     path("news/", views.NewsPageView.as_view(), name="news"),
     path("news/<int:pk>/", views.NewsPageDetailView.as_view(), name="news_detail"),
-    path("courses_list/", views.CoursesListView.as_view(), name="courses"),
+    path("courses/", views.CoursesListView.as_view(), name="courses"),
     path(
         "courses/<int:pk>/",
         views.CoursesDetailView.as_view(),
@@ -17,5 +17,4 @@ urlpatterns = [
     ),
     path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
     path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
-    path("login/", views.LoginPageView.as_view(), name="login"),
 ]
