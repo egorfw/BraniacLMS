bd9434f5fc12cc9dca439e2de201a8396f307cb4
egorfw <egorfw@outlook.com>
2023-02-28 19:46:05 +0300

    lesson_6 task 5 news date filter

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -7,7 +7,7 @@ from mainapp import models as mainapp_models
 @admin.register(mainapp_models.News)
 class NewsAdmin(admin.ModelAdmin):
     search_fields = ["title", "preambule", "body"]
-
+    list_filter = ["created", "updated", "deleted"]
 
 @admin.register(mainapp_models.Lesson)
 class LessonAdmin(admin.ModelAdmin):
